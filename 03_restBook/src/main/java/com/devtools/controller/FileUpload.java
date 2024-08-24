f87da49eb1d7f1856faf2983031014ca6682b09f
anshawasthi01office <anshawasthi.office@gmail.com>
2024-08-24 16:11:07 +0530

    Image Uploading to dynamic path | Serving image file

--- 03_restBook/src/main/java/com/devtools/controller/FileUpload.java
+++ 03_restBook/src/main/java/com/devtools/controller/FileUpload.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devtools.helper.FileUploadHelper;
 
@@ -38,7 +39,8 @@ public class FileUpload {
 //		file upload code........
 		boolean f = fileUploadHelper.uploadFile(file);
 		if(f) {
-			return ResponseEntity.ok("File is successfully uploaded");
+//			return ResponseEntity.ok("File is successfully uploaded");
+			return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(file.getOriginalFilename()).toUriString());
 		}
 		
 		
