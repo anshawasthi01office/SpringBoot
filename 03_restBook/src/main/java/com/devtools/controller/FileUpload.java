368695297181a316a592bb745adfa7f92fca5ad2
anshawasthi01office <anshawasthi.office@gmail.com>
2024-08-24 15:17:05 +0530

    File Uploading-Static Path

---
+++ 03_restBook/src/main/java/com/devtools/controller/FileUpload.java
@@ -0,0 +1,55 @@
+package com.devtools.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.devtools.helper.FileUploadHelper;
+
+@RestController
+public class FileUpload {
+	
+	@Autowired
+	private FileUploadHelper fileUploadHelper;
+	
+	@PostMapping("/upload-file")
+	public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file) {
+//		System.out.println(file.getOriginalFilename());
+//		System.out.println(file.getSize());
+//		System.out.println(file.getContentType());
+//		System.out.println(file.getName());
+		
+		try {
+
+		if(file.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("request must contain file");
+		}
+		
+		if(!file.getContentType().equals("image/jpeg"))
+		{
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("request must contain file");
+		}
+		
+		
+//		file upload code........
+		boolean f = fileUploadHelper.uploadFile(file);
+		if(f) {
+			return ResponseEntity.ok("File is successfully uploaded");
+		}
+		
+		
+		
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong try again");
+	}
+	
+
+}
