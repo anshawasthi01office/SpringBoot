9761edec186f50d89a62d05c883d790c2fb55fbc
anshawasthi01office <anshawasthi.office@gmail.com>
2024-08-24 12:31:52 +0530

    Nesting Custom Type in spring Boot Rest Api | Using ONE TO ONE Mapping with Spring Boot Rest API

--- 03_restBook/src/main/java/com/devtools/entities/Book.java
+++ 03_restBook/src/main/java/com/devtools/entities/Book.java
@@ -1,8 +1,10 @@
 package com.devtools.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,14 +19,17 @@ public class Book {
 	private int id;
     
 	private String title;
-	private String author;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
+	
 	public int getId() {
 		return id;
 	}
 	public Book() {
 		// TODO Auto-generated constructor stub
 	}
-	public Book(int id, String title, String author) {
+	public Book(int id, String title, Author author) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -43,10 +48,10 @@ public class Book {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getAuthor() {
+	public Author getAuthor() {
 		return author;
 	}
-	public void setAuthor(String author) {
+	public void setAuthor(Author author) {
 		this.author = author;
 	}
 
